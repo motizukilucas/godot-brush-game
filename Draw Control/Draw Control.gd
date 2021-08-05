fba6878ed41eb6296537ac225de2819c1e4f69ea
Lucas Motizuki <motizuki@protonmail.com>
2021-08-04 21:48:04 -0300

    Making the body and paint disappear together

--- Draw Control/Draw Control.gd
+++ Draw Control/Draw Control.gd
@@ -1,40 +1,36 @@
 extends Node2D
 
 var paint_list = []
-var body_list = []
 
 var last_mouse_pos = Vector2()
 
-func _process(_delta):
+func _process(delta):
 	var mouse_pos = get_viewport().get_mouse_position()
 
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		if mouse_pos.distance_to(last_mouse_pos) >= 1:
 			add_paint(mouse_pos)
-			add_body(mouse_pos)
-
+			
 	last_mouse_pos = mouse_pos
 
 func _draw():
 	for dot in paint_list:
 		draw_circle(dot.pos, 32, Color(0,0,0))
 		
-	for body in body_list:
 		var timer = Timer.new()
-		body.add_child(timer)
+		dot.body.add_child(timer)
 		timer.set_wait_time(5)
-		timer.connect("timeout", self, "_on_Timer_timeout", [body])
+		timer.connect("timeout", self, "_on_Timer_timeout", [dot])
 		
-		add_child(body)
+		add_child(dot.body)
 		
 		timer.start()
 
-func _on_Timer_timeout(body):
-	body_list.erase(body)
-	body.queue_free()
+func _on_Timer_timeout(dot):
+	paint_list.erase(dot)
+	dot.body.queue_free()
 	
-	body_list.clear()	
-	paint_list.clear()
+#	paint_list.clear()
 	update()
 
 func add_paint(mouse_pos):
@@ -42,10 +38,6 @@ func add_paint(mouse_pos):
 
 	dot.pos = mouse_pos
 
-	paint_list.append(dot)
-	update()
-
-func add_body(mouse_pos):
 	var body = StaticBody2D.new()
 	var shape = CircleShape2D.new()
 	shape.set_radius(32)
@@ -53,6 +45,8 @@ func add_body(mouse_pos):
 	collision.set_shape(shape)
 	body.set_position(mouse_pos)
 	body.add_child(collision)
-		
-	body_list.append(body)
+	dot.body = body
+	
+	paint_list.append(dot)
+	update()
 	
